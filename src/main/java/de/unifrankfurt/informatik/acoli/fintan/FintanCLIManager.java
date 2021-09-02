2a3f48234861a6fd4a82c6045fc2a12c54196f4f
cfaeth <Faeth@em.uni-frankfurt.de>
2021-09-02 17:21:36 +0200

    renamed FintanManager in core. minor updated to apertium-demo.json

--- src/main/java/de/unifrankfurt/informatik/acoli/fintan/FintanCLIManager.java
+++ src/main/java/de/unifrankfurt/informatik/acoli/fintan/FintanCLIManager.java
@@ -4,7 +4,7 @@ package de.unifrankfurt.informatik.acoli.fintan;
 public class FintanCLIManager {
 
 	public static void main(String[] args) throws Exception {
-		de.unifrankfurt.informatik.acoli.fintan.core.FintanCLIManager.main(args);
+		de.unifrankfurt.informatik.acoli.fintan.core.FintanManager.main(args);
 	}
 
 }
