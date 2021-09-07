33724e2fbcddffc75626cbe7237f4824d19cad11
cfaeth <Faeth@em.uni-frankfurt.de>
2021-09-07 17:03:39 +0200

    added simple quick start guide

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # store the path of the root directory in HOME
 HOME=$( dirname -- "$(realpath -- "$0")");
 
+cd $HOME
 
 mvn exec:java -Dfile.encoding=UTF8 -Dexec.mainClass=de.unifrankfurt.informatik.acoli.fintan.FintanCLIManager -Dexec.args="$*"
 
