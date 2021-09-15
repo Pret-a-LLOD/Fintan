a619104f670d748798623ea215bd09d645b717f5
cfaeth <Faeth@em.uni-frankfurt.de>
2021-09-15 22:47:27 +0200

    modified build script to skip internal tests within submodule poms

--- build.sh
+++ build.sh
@@ -2,17 +2,21 @@
 
 # store the path of the conll-rdf directory in HOME
 HOME=$( dirname -- "$(realpath -- "$0")");
+
 cd $HOME
 git clone https://github.com/acoli-repo/fintan-core.git
 cd fintan-core
-mvn clean install
+mvn --batch-mode --quiet -DskipTests clean install
+
 cd $HOME
 git clone https://github.com/acoli-repo/conll-rdf.git
 cd conll-rdf
-mvn clean install
+mvn --batch-mode --quiet -DskipTests clean install
+
 cd $HOME
 git clone https://github.com/tarql/tarql.git
 cd tarql
-mvn clean install
+mvn --batch-mode --quiet -DskipTests clean install
+
 cd $HOME
-mvn clean install
+mvn --batch-mode clean install
