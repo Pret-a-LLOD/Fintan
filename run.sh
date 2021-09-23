a48c08c7ea3ad19a084585647effbf5b85b5be5d
cfaeth <Faeth@em.uni-frankfurt.de>
2021-09-23 12:12:11 +0200

    fixed main class in run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ HOME=$( dirname -- "$(realpath -- "$0")");
 
 cd $HOME
 
-mvn exec:java --quiet -e -Dfile.encoding=UTF8 -Dexec.mainClass=de.unifrankfurt.informatik.acoli.fintan.FintanCLIManager -Dexec.args="$*"
+mvn exec:java --quiet -e -Dfile.encoding=UTF8 -Dexec.mainClass=org.acoli.fintan.FintanCLIManager -Dexec.args="$*"
 
